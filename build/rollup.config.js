b8fd1e5c5130d140ffba2eb18a2a44d5ffff12c0
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-04 11:48:34 -0800

    update babel configuration, uses babel-preset-env

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -10,15 +10,9 @@ export default {
   plugins: [
     vue(),
     babel({
-      babelrc: false,
       runtimeHelpers: true,
       externalHelpers: false,
       exclude: 'node_modules/**',
-      presets: [['es2015', {'modules': false}]],
-      plugins: [
-        'transform-object-assign',
-        'external-helpers'
-      ]
     }),
     (process.env.NODE_ENV === 'production' && uglify())
   ],
