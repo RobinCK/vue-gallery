7396fd6e02a4bdc50b26b04e92f6a1a46802bc5a
Norman Sander <norman.sander@fromatob.com>
2019-02-12 16:27:43 +0100

    fix: set node_env to production for both builds

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,12 +3,12 @@ import babel from 'rollup-plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 import commonjs from 'rollup-plugin-commonjs';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isMinify = process.env.BUILD_MODE === 'minify';
 
 export default {
   input: 'src/index.js',
   output: {
-    file: isProduction ? 'dist/js/vue-gallery.min.js' : 'dist/js/vue-gallery.js',
+    file: isMinify ? 'dist/js/vue-gallery.min.js' : 'dist/js/vue-gallery.js',
     format: 'umd',
     name: 'VueGallery',
   },
@@ -21,6 +21,6 @@ export default {
       runtimeHelpers: true,
       externalHelpers: false,
     }),
-    (isProduction && terser())
+    (isMinify && terser())
   ],
 };
